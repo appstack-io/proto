7aecf5831d649fd20a4278940ebe16b529c1c41b
orgal <or.gal@vee.com>
2023-12-09 21:37:42 +0200

    fix: generateServiceProtos.ts

---
+++ src/generateServiceProtos.ts
@@ -0,0 +1,18 @@
+import * as fs from 'fs';
+import { combineProtos } from './combineProtos';
+
+export async function generateServiceProtos(dir: string) {
+  const moduleFiles = fs
+    .readdirSync(dir)
+    .filter((file) => file.endsWith('.ts'))
+    .map((file) => `${dir}/${file}`);
+  for (const file of moduleFiles) {
+    const clss = await import(file);
+    const imports = clss['getImports'] ? clss.getImports() : null;
+    if (!imports) continue;
+    const sourceDirs = imports
+      .map((imp) => (imp['getDirname'] ? imp['getDirname']() : null))
+      .filter((i) => i);
+    combineProtos(sourceDirs, `${file}.proto`);
+  }
+}
