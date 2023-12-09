fcabc1e35b0f3fc0804c1dd59b5e8ede56de4ca3
orgal <or.gal@vee.com>
2023-12-09 21:41:23 +0200

    fix: generateServiceProtos.ts

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './combineProtos';
 export * from './consolidateProtos';
 export * from './generateInterfaces';
 export * from './generateClient';
+export * from './generateServiceProtos';
