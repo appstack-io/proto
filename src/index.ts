d1908170962574745deb65a60e115608e2fbf780
orgal <or.gal@vee.com>
2023-12-16 19:05:06 +0200

    fix: revamped all scripts and added functionality that was spread out in other projects

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export * from './combine-protos';
 export * from './consolidate-protos';
 export * from './generate-interfaces';
 export * from './generate-client';
+export * from './generate-grpc-client';
 export * from './generate-module-protos';
 export * from './fetch-protos';
