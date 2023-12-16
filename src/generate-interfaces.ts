2ae6f49dc688d7cd42bedf367c12f7e839db2a0e
orgal <or.gal@vee.com>
2023-12-16 20:39:05 +0200

    fix: generate interfaces bug

--- src/generate-interfaces.ts
+++ src/generate-interfaces.ts
@@ -1,8 +1,15 @@
 export function generateInterfaces(source: string) {
   const interfaceRegex = /export (interface|enum) [\s\S]*?\}(?=\n|$)/gm;
   const imports: string[] = [
-    "import { Empty } from './google/protobuf/empty';",
-    "import { Observable } from 'rxjs';",
+    `import { Empty } from './google/protobuf/empty';`,
+    `import { Observable } from 'rxjs';`,
+    `import type { CallContext, CallOptions, CallOptionsExt } from "nice-grpc-common";`,
+    `type DeepPartial<T> = T extends Builtin ? T
+      : T extends globalThis.Array<infer U> ? globalThis.Array<DeepPartial<U>>
+      : T extends ReadonlyArray<infer U> ? ReadonlyArray<DeepPartial<U>>
+      : T extends {} ? { [K in keyof T]?: DeepPartial<T[K]> }
+      : Partial<T>;
+    `,
   ];
   const interfaces: string[] | null = source.match(interfaceRegex);
   return [...imports, ...(interfaces || [])].join('\n\n');
