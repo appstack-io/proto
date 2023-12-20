26212d7e35fc26f24a22f417cc10ddd0f9efe6e3
orgal <or.gal@vee.com>
2023-12-20 14:20:13 +0200

    fix: fetchProtos

--- src/fetch-protos.ts
+++ src/fetch-protos.ts
@@ -1,44 +1,46 @@
 import * as fs from 'fs';
-import * as https from 'https';
+import { Octokit } from 'octokit';
 
-function downloadFile(url: string, dest: string): Promise<void> {
-  return new Promise((resolve, reject) => {
-    const file = fs.createWriteStream(dest);
-
-    https
-      .get(url, (response) => {
-        if (response.statusCode !== 200) {
-          reject(`Failed to download file: Status Code ${response.statusCode}`);
-          return;
-        }
-
-        response.pipe(file);
-
-        file.on('finish', () => {
-          file.close();
-          resolve();
-        });
-      })
-      .on('error', (err) => {
-        fs.unlink(dest, () => {
-          return;
-        });
-        reject(`Failed to download file: ${err.message}`);
-      });
+async function downloadFile(
+  repo: string,
+  path: string,
+  dest: string,
+  token: string,
+): Promise<void> {
+  const octokit = new Octokit({
+    auth: token,
   });
+  const result = await octokit.request(
+    `GET /repos/{owner}/{repo}/contents/{path}`,
+    {
+      owner: repo.split('/')[0],
+      repo: repo.split('/')[1],
+      path: path,
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28',
+      },
+    },
+  );
+  const file = Buffer.from((result.data as any).content, 'base64').toString();
+  fs.writeFileSync(dest, file, 'utf8');
 }
 
 export async function fetchProtos(
   services: Record<string, { repository: string; branch: string }>,
   target: string,
+  token: string,
 ) {
   await Promise.all(
-    Object.keys(services).map((key) => {
+    Object.keys(services).map(async (key) => {
       const service = services[key];
       const { repository, branch } = service;
-      const url = `https://raw.githubusercontent.com/${repository}/${branch}/src/combined.proto`;
       fs.mkdirSync(`${target}/fetched-protos/${key}`, { recursive: true });
-      return downloadFile(url, `${target}/fetched-protos/${key}/fetched.proto`);
+      return await downloadFile(
+        repository,
+        'src/combined.proto',
+        `${target}/fetched-protos/${key}/fetched.proto`,
+        token,
+      );
     }),
   );
 }
--- src/tests/combine-protos.spec.ts
+++ src/tests/combine-protos.spec.ts
@@ -2,7 +2,7 @@ import { combineProtos } from '../combine-protos';
 
 describe('combine-protos', () => {
   test('basic', async () => {
-    const combined = combineProtos([`${__dirname}`], []);
+    const combined = combineProtos([`${__dirname}/temp/protos`], []);
     expect(combined).toMatchSnapshot();
   });
 });
